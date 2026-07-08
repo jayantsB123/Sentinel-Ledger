b257ce5e1b35828d37be416d31b39856f5604f14
jayantsB123 <jayantsing123@gmail.com>
2026-07-08 17:43:31 +0530

    feature/payment-state-machine: creates new enums for global constants

---
+++ src/main/java/com/jayant/payment/Sentinel_Ledger/model/enums/Currency.java
@@ -0,0 +1,5 @@
+package com.jayant.payment.Sentinel_Ledger.model.enums;
+
+public enum Currency {
+    INR, USD, EUR, GBP
+}
---
+++ src/main/java/com/jayant/payment/Sentinel_Ledger/model/enums/EntryType.java
@@ -0,0 +1,5 @@
+package com.jayant.payment.Sentinel_Ledger.model.enums;
+
+public enum EntryType {
+    DEBIT, CREDIT
+}
